9c11c293079af28f0e01729aca988cdb0744e34e
Jethro_Block <jethroblock22@gmail.COM>
2023-10-10 14:53:48 +0100

    create users routes file

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const router = express.Router()
+const userSchema = require("models/user")
+
+// Getting All
+
+// Getting One
+
+// Creating One
+
+// Updating One
+
+// Deleting One
