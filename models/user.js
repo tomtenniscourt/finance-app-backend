fbb574da019c526a795c0a61e77e5aeca7d0a847
Catherinen29 <cat_nevin@hotmail.com>
2023-10-12 14:52:50 +0100

    Add to User Schema.

--- models/user.js
+++ models/user.js
@@ -1,14 +1,26 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-    name: {
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
         type: String,
         required: true
     },
     email: {
         type: String,
         required: true
+    }, 
+    password: {
+        type: password,
+        required: true
+    },
+    portfolioId: {
+        type: [{ type: portfolioSchema }],
+        required: true
     }
 })
 
-module.exports = mongoose.model("users", userSchema)
+module.exports = mongoose.model("users", userSchema, "users")
