69bba3d5dd1f651ea3dd39246895eb4807f53dd2
Jethro_Block <jethroblock22@gmail.COM>
2023-10-12 11:53:02 +0100

    add get user via id function

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,22 @@ const express = require("express")
 const router = express.Router()
 const userSchema = require("../models/user")
 
+const getUserViaId = async (req,res,next) => {
+    let user;
+    try {
+        // find the user via their id
+        user = await userSchema.findById(req.params.id)
+        // if there is no user id, then return an error
+        if (user == null) {
+            return res.status(404).json({message: "Can't find user"})
+        }
+    } catch (err) {
+        return res.status(500).json({message: err.message})
+    }
+    res.user = user
+    next()
+}
+
 // Getting All
 router.get("/", async (req,res) => {
     try {
@@ -14,6 +30,9 @@ router.get("/", async (req,res) => {
 })
 
 // Getting One
+router.get("/:id", getUserViaId, (req,res) => {
+    res.send(res.user.name)
+})
 
 // Creating One
 router.post("/", async (req,res) => {
