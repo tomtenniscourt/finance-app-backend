a6821a5b6edc7681a0e29af9f0a4bf39190ee3c2
Catherinen29 <cat_nevin@hotmail.com>
2023-11-01 17:18:48 +0000

    Create refreshTokenSchema.

---
+++ models/refreshTokenSchema.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const refreshTokenSchema = new mongoose.Schema({
+    email: {
+        type: String,
+    },
+    token: {
+        type: String
+    }
+})
+
+module.exports = mongoose.model("refreshToken", refreshTokenSchema, "refreshToken")
